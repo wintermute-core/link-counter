5aafea984f6957b0cc18fa1ccc16d5efe54b8725
Denis O <denis.o@linux.com>
2022-10-27 12:18:12 +0300

    Add function to count links

--- links/links_test.go
+++ links/links_test.go
@@ -6,22 +6,18 @@ import (
 )
 
 func TestCountLinksSingle(t *testing.T) {
-
 	stats := CountLinks("abc.com",
 		[]string{
 			"http://xyz.com",
 			"http://abc.com",
+			"http://abc.com/about.html",
 			"http://111.com",
 		})
 
-	assert.Equal(t, 1, stats.Internal)
-	assert.Equal(t, 2, stats.External)
-	assert.Equal(t, 0, stats.Error)
-
+	validateStats(t, stats, 2, 2, 0)
 }
 
 func TestCountLinksDifferentSchemas(t *testing.T) {
-
 	stats := CountLinks("abc.com",
 		[]string{
 			"http://abc.com",
@@ -32,8 +28,36 @@ func TestCountLinksDifferentSchemas(t *testing.T) {
 			"https://2.com/",
 		})
 
-	assert.Equal(t, 3, stats.Internal)
-	assert.Equal(t, 3, stats.External)
-	assert.Equal(t, 0, stats.Error)
+	validateStats(t, stats, 3, 3, 0)
+}
 
+func TestCountLinksRelativePath(t *testing.T) {
+	stats := CountLinks("abc.com",
+		[]string{
+			"/",
+			"..",
+			".",
+			"qwe.html",
+		})
+
+	validateStats(t, stats, 4, 0, 0)
+}
+
+func TestCountLinksErrors(t *testing.T) {
+	stats := CountLinks("abc.com",
+		[]string{
+			"http//abc.com",
+			"🐛U+1F41B\n",
+			"http://abc.com/a.html",
+			"http://xyz.com",
+			"http://xyz.com/🐛.txt\t",
+		})
+
+	validateStats(t, stats, 2, 1, 2)
+}
+
+func validateStats(t *testing.T, stats LinkStats, internal, external, error int) {
+	assert.Equalf(t, internal, stats.Internal, "Expected to have %d internal links", internal)
+	assert.Equalf(t, external, stats.External, "Expected to have %d external links", external)
+	assert.Equalf(t, error, stats.Error, "Expected to have %d errors", error)
 }
